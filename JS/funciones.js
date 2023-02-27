16b046d131cffcdea5c859717b1eda01e58dc0e9
Micaela Roig Courtis <mmrc1992@gmail.com>
2023-02-27 19:19:07 -0300

    Quinta escritura en el repositorio git - Proyecto Ticmas - Estudiante Micaela Roig Courtis

--- JS/funciones.js
+++ JS/funciones.js
@@ -1,23 +1,31 @@
 function DirigirseALaSección (Presentación) {
     location.hash = "#" + Presentación
-}
+};
 
 function DirigirseALaSección (Información_básica) {
     location.hash = "#" + Información_básica
-}
+};
 
 function DirigirseALaSección (Formación) {
     location.hash = "#" + Formación
-}
+};
 
 function DirigirseALaSección (Experiencia_laboral) {
     location.hash = "#" + Experiencia_laboral
-}
+};
 
 function DirigirseALaSección (Tecnología) {
     location.hash = "#" + Tecnología
-}
+};
 
 function DirigirseALaSección (Habilidades_blandas) {
     location.hash = "#" + Habilidades_blandas
+};
+
+function cambiar() {
+    document.getElementById('Fondo').style.backgroundColor= '#F0C499';
+}
+
+function retornarcolor() {
+    document.getElementById('Fondo').style.backgroundColor= '#d1c4b9';
 }
