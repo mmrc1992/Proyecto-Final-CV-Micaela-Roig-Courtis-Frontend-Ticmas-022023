c89ade0931c38b865addf97c92e77b2786b76402
Micaela Roig Courtis <mmrc1992@gmail.com>
2023-02-27 06:09:12 -0300

    Tercera escritura en el repositorio - Proyecto Frontend Ticmas - Estudiante Micaela Roig Courtis

---
+++ JS/funciones.js
@@ -0,0 +1,23 @@
+function DirigirseALaSección (Presentación) {
+    location.hash = "#" + Presentación
+}
+
+function DirigirseALaSección (Información_básica) {
+    location.hash = "#" + Información_básica
+}
+
+function DirigirseALaSección (Formación) {
+    location.hash = "#" + Formación
+}
+
+function DirigirseALaSección (Experiencia_laboral) {
+    location.hash = "#" + Experiencia_laboral
+}
+
+function DirigirseALaSección (Tecnología) {
+    location.hash = "#" + Tecnología
+}
+
+function DirigirseALaSección (Habilidades_blandas) {
+    location.hash = "#" + Habilidades_blandas
+}
