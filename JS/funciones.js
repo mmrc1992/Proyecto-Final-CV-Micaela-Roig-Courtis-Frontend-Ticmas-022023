19a7cdd9da6336d637341609bff8997323628d6c
Micaela Roig Courtis <mmrc1992@gmail.com>
2023-02-27 22:49:02 -0300

    Octava escritura en el repositorio git - Proyecto Frontend Ticmas - Estudiante Micaela Roig Courtis

--- JS/funciones.js
+++ JS/funciones.js
@@ -2,30 +2,39 @@ function DirigirseALaSección (Presentación) {
     location.hash = "#" + Presentación
 };
 
+
 function DirigirseALaSección (Información_básica) {
     location.hash = "#" + Información_básica
 };
+
 
 function DirigirseALaSección (Formación) {
     location.hash = "#" + Formación
 };
 
+
 function DirigirseALaSección (Experiencia_laboral) {
     location.hash = "#" + Experiencia_laboral
 };
+
 
 function DirigirseALaSección (Tecnología) {
     location.hash = "#" + Tecnología
 };
 
+
 function DirigirseALaSección (Habilidades_blandas) {
     location.hash = "#" + Habilidades_blandas
 };
 
+
 function cambiar() {
-    document.getElementById('Fondo').style.backgroundColor= '#F0C499';
-}
+    document.getElementById('Fondo').style.backgroundColor= '#000000';
+    document.getElementById('NombreYApellido').style.color= '#F6F2EF';
+};
+
 
 function retornarcolor() {
     document.getElementById('Fondo').style.backgroundColor= '#d1c4b9';
-}
+    document.getElementById('NombreYApellido').style.color= '#424242';
+};
